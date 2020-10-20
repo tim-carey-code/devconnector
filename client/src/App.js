7ce7b9c28ebe2bd7a78cf4179eecbde78889998e
tim carey <timcarey1989@gmail.com>
2020-10-20 13:48:22 -0400

    working on frontend auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "../src/components/layout/Navbar";
@@ -6,11 +6,21 @@ import Landing from "../src/components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <>
       <Provider store={store}>
